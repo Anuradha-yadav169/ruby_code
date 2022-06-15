c56def053cac4dfe569211d2182752357479bbc1
anuradha <you@example.com>
2022-06-15 12:04:22 +0530

    Indentation changes

--- map_function.rb
+++ map_function.rb
@@ -1,7 +1,7 @@
-# array = %w[1, 2, 3, 4, 5]
+# array = %w(1 2 3 4 5)
 # a = array.map { |n| n * 2 }
 # puts a
 
-array = %w[1, 2, 3, 'a', 'b']
+array = %w(1 2 3 a b)
 a = array.map { |n| n * 2 }
 puts a
